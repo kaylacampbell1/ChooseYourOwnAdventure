992e4769e4959c74451115f93e6fd031e5f50580
Kayla Campbell <campbellkayla2003@gmail.com>
2019-06-05 20:31:07 +0000

    final

--- js/game.js
+++ js/game.js
@@ -6,40 +6,94 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "Sunday night you are invited to your friend's party but you also have homework.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Finish homework",
+                    nextLevel: "pass",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Party!",
+                    nextLevel: "fun",
                 },
+                {
+                    text: "Watch netflix",
+                    nextLevel: "nice"
+                }
             ]
         },
 
-        cave: {
+        pass: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "Congratulations you got an A!",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Play again",
                     nextLevel: "start",
                 },
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        fun: {
+            message: "You enjoyed your night but failed to complete your homework",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Copy from a friend",
+                    nextLevel: "copied",
+                },
+               
+                {
+                    text: "Take the F",
+                    nextLevel: "fail",
+                },
+            ]
+        },
+        
+        copied: {
+            message: "I'm disappointed in you, cheating is never the answer.",
+            choices: [
+                {
+                    text: "Try again",
                     nextLevel: "start",
                 },
             ]
+        },
+        
+        nice: {
+            message: "You now get to tell your friend about this wonderful show on netflix",
+            choices: [
+                {
+                    text: "Do homework",
+                    nextLevel: "tried",
+                },
+                {
+                    text: "Go to bed",
+                    nextLevel: "fail",
+                },
+            ]
+        },
+        
+        fail: {
+            message: "You made some pretty bad decisions today",
+            choices: [
+                {
+                  text: "Try again",
+                  nextLevel: "start",
+                },
+                ]
+        },
+        
+        tried: {
+            message: "You slacked off a bit but got some work done. You recieved a C on your assignment.",
+            choices: [
+                {
+                  text: "Try again",
+                  nextLevel: "start"
+                },
+            ]
+            
         },
 
     }
